76315813bfcfbdb085fc905a9b6ed7b2ec73e483
Medard Dziedzic <medarddziedzic@gmail.com>
2022-03-28 14:21:02 +0200

    added allowed hosts

--- test_stxnext/settings.py
+++ test_stxnext/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zbfn)cci2oy1gmkr$om%$0!!$qx^j)-te=6md$+j#4xq*&_k8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://tranquil-plateau-33840.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
